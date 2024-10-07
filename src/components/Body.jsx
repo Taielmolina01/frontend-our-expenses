212d8ea8f5d02769f7d88ae29cc78a83644c38a7
Taielmolina01 <tamolina@fi.uba.ar>
2024-10-07 16:19:44 -0300

    first commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,24 @@
+import logo from '../assets/logo.png'
+
+function Body() {
+    return (
+        <div class="body-container">
+            <div class="container">
+                <div className="text-container">
+                   <h2>
+                        OurExpenses is coming soon!
+                   </h2>
+                   <p>
+                        We are developing a new app to split your expenses with your friends with a minimalist design based on the last trends!
+                   </p>
+                   <p>
+                        We are going to introduce the app to you in a desktop version but we are already working on a mobile app for Android and iOS
+                   </p>
+                   <image src={logo} alt="Logo de OurExpenses"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+    return (
+        <nav>
+            <div class="enterprise-name">
+                OurExpenses
+            </div>
+            <div class="on-right">
+                <button class="sign-in">
+                    Iniciar sesión
+                </button>
+                <button class="sign-up">
+                    Registrarse
+                </button>
+            </div>
+        </nav>
+    )
+}   
+
+export default Navbar;
