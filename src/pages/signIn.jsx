8a438529d553a8df7cb37ed4b3acd5f72c72ac52
Taielmolina01 <tamolina@fi.uba.ar>
2024-10-08 12:07:43 -0300

    add the handleSubmits and the types of user in the back are corrected now

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -2,16 +2,62 @@ import Footer from "../components/Footer";
 import NavbarLog from "../components/NavbarLog";
 
 function SignIn() {
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+
+        const json = {};
+
+        json["mail"] = formData.mail;
+    
+        let res;
+
+        try {
+            res = await fetch(`${BACK_URL}/users`, {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(json),
+            });
+
+            const data = await res.json();
+      
+            if (!res.ok) {
+                setError("No hay un usuario con ese mail registrado")
+            } else {
+                if (data.password === formData.password) {
+                    router.push("/users/" + data.mail);
+                } else {
+                    setError("La contraseña es incorrecta")
+                }
+            }
+          } catch (e) {
+            setError("Error de conexión");
+            return;
+          }
+    }
+
     return (
         <>
             <NavbarLog />
             <div class="form-container">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h2>Sign in your account</h2>
                     <label>Mail</label>
-                    <input type="email"/>
+                    <input 
+                        type="email"
+                        value={formData.mail}
+                        required
+                    />
                     <label>Password</label>
-                    <input type="password"/>
+                    <input 
+                        type="password"
+                        value={formData.password}
+                        required
+                    />
                     <input type="submit" value="Sign in"/>
                 </form>
             </div>
--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,19 +1,79 @@
 import Footer from "../components/Footer";
 import NavbarLog from "../components/NavbarLog";
 
+const BACK_URL = "";
+
 function SignUp() {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+
+        if (formData.password !== formData.confirmPassword) {
+            setError('Las contraseñas no coinciden');
+            return;
+        }
+        
+        const json = {};
+
+        json["mail"] = formData.mail;
+        json["name"] = formData.name;
+        json["password"] = formData.password;
+    
+        let res;
+
+        try {
+            res = await fetch(`${BACK_URL}/users`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(json),
+            });
+
+            const data = await res.json();
+      
+            if (!res.ok) {
+                setError("Atrapé algun error")
+            } else {
+                router.push("/users/" + data.mail);
+            }
+          } catch (e) {
+            setError("Error de conexión");
+            return;
+          }
+    }   
+
     return (
         <>
             <NavbarLog />
             <div class="form-container">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h2>Sign up</h2>
+                    <label>Enter your name</label>
+                    <input 
+                        type="string"
+                        value="formData.name"
+                        required
+                    />
                     <label>Enter your mail</label>
-                    <input type="email"/>
+                    <input 
+                        type="email"
+                        value="formData.mail"
+                        required
+                    />
                     <label>Create a password</label>
-                    <input type="password"/>
+                    <input 
+                        type="password"
+                        value="formData.password"
+                        required
+                    />
                     <label>Repeat the password</label>
-                    <input type="password"/>
+                    <input 
+                        type="formData.confirmPassword"
+                        required
+                    />
+                    {error && <p style={{ color: 'red' }}>{error}</p>}
                     <input type="submit" value="Sign up"/>
                 </form>
             </div>
